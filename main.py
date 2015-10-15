a9f669bc0844b71502d34a1f212fbc48db5d360d
DANIEL RIVERO <darthquevedus@gmail.com>
2015-10-15 13:08:04 +0200

    format changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 import tools_deltaf as tools
 import os, glob
-file_log = raw_input(' Filename of the g09 output (including log) \n, remember, should be a single point TDDFT calculation \n')
-incr = float(raw_input('value of chosen increment for f gradient calculation: \n'))
+file_log = raw_input(' Filename of the g09 output (including log) \nremember: should be a single point TDDFT calculation. \n')
+incr = float(raw_input('Value of chosen increment for f gradient calculation: \n'))
 
 tools.writer_inputs(file_log,incr)
 
-print 'g09 input files were generated, have fun \n' 
+print 'G09 input files were generated, have fun \n' 
 answ = int(raw_input(' Type \n 0 if u do not want to launch all the generated jobs or \n 1 if u want to launch them \n'))
 
 if answ == 1:
